81ed5b4c62cab3ceee34993a31c83c5b0b251fa1
Andrew <andrewcgraves@gmail.com>
2022-10-11 22:50:11 -0700

    working with generative UI

--- Assets/Scripts/ContractSpawner.cs
+++ Assets/Scripts/ContractSpawner.cs
@@ -14,8 +14,23 @@ public class ContractSpawner : MonoBehaviour
         Debug.Log("Spawner Started");
         contracts.Add(new Contract("contract1", 19, 100, 0));
         var newTransform = transform;
+
+                GameObject contract = Instantiate(prefab, newTransform.position, Quaternion.identity);
+
+
+        // foreach (var item in contracts)
+        // {
+        //     // Create the game object, add to array with the ID
+        // }
+        //  ThisButton.GetComponentInChildren(Text).text = "testing";
+
+         // When we create buttons, we need to pass them additional information about the context they exist in.
+         // They should return the ID of the contract that was clicked
+         // Inorder to return this, we need to link the contractID with the data to the actual type
         
-        GameObject contract = Instantiate(prefab, newTransform.position, Quaternion.identity);
     }
 
+    void onContractClick() {
+
+    }
 }
---
+++ Assets/Scripts/ListController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListController : MonoBehaviour
+{
+
+    public GameObject ContentPanel;
+    public GameObject ListItemPrefab;
+
+    // public ContractController cc;
+    private List<Contract> contracts = new List<Contract>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contracts.Add(new Contract("contract1", 19, 100, 0));
+        contracts.Add(new Contract("contract2", 20, 100, 0));
+        contracts.Add(new Contract("contract3", 22, 100, 0));
+        contracts.Add(new Contract("contract4", 15, 100, 0));
+
+        Debug.Log("Contracts added");
+
+        foreach (Contract contract in contracts)
+        {
+            GameObject newContract = Instantiate(ListItemPrefab) as GameObject;
+            ListItemController controller = newContract.GetComponent<ListItemController>();
+            controller.Name.text = contract.GetName();
+            controller.Description.text = contract.GetGuid();
+            // newContract.transform.parent = ContentPanel.transform;
+            // newContract.transform.localScale = Vector3.one;
+        }
+    }
+}
