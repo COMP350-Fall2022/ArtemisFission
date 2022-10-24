ad3aafff46f7cb0a5db0b5d7aa1ce84b2e3a1cff
Frances637 <89601535+Frances637@users.noreply.github.com>
2022-10-24 14:46:08 -0700

    Updated Code
    I debugged the code to make it error free.

--- Assets/Scripts/EmployeeDisplayUI.cs
+++ Assets/Scripts/EmployeeDisplayUI.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EmployeeDisplayUI : MonoBehaviour
 {
-    public Text nameText = "EmployeeName";
-    public Text salaryText = "500,000";
-    public Text moraleText = "5";
-    public Text speedText "4";
-    public Text techText "3"; 
+    public Text nameText;
+    public Text salaryText;
+    public Text moraleText;
+    public Text speedText;
+    public Text techText; 
     // Start is called before the first frame update
     void Start()
     {
-        //nameText.text = "EmployeeName";
+        nameText.text = "EmployeeName";
+        salaryText.text = "500,000";
+        moraleText.text = "5";
+        speedText.text = "4";
+        techText.text = "3";
     }
 
     // Update is called once per frame
