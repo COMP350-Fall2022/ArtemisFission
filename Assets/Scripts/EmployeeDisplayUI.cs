fceca5eed93e81ad6a7bcca31f8a9fe206326436
Frances637 <89601535+Frances637@users.noreply.github.com>
2022-10-24 14:38:50 -0700

    Code in Employee Display UI
    I added code to the scripts.

--- Assets/Scripts/EmployeeDisplayUI.cs
+++ Assets/Scripts/EmployeeDisplayUI.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class EmployeeDisplayUI : MonoBehaviour
 {
-    //public Text nameText;
+    public Text nameText = "EmployeeName";
+    public Text salaryText = "500,000";
+    public Text moraleText = "5";
+    public Text speedText "4";
+    public Text techText "3"; 
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,10 @@ public class EmployeeDisplayUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        nameText.text = "Charon";
+        salaryText.text = "200,000";
+        moraleText.text = "7";
+        speedText.text = "6";
+        techText.text =  "5";
     }
 }
