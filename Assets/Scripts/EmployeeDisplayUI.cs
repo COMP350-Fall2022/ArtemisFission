58030a8ab12154af7d916207e7f8cc568e95ded7
Frances637 <89601535+Frances637@users.noreply.github.com>
2022-10-24 14:12:19 -0700

    Art assets (#55)
    * I worked on a mockup screen for the Employee Display UI.
    
    * oop
    
    * EmployeeDisplayUI
    
    Employee Display UI scripts, I need some help with this because it has some bugs. 
    
    Signed-off-by: Frances637 <89601535+Frances637@users.noreply.github.com>
    
    * Resources Display mockup
    
    I worked on the Resource Display mockup.
    
    * art go woosh
    
    * last second things
    
    Signed-off-by: Frances637 <89601535+Frances637@users.noreply.github.com>
    Co-authored-by: andrew (m) <andrewcgraves@gmail.com>

---
+++ Assets/Scripts/EmployeeDisplayUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmployeeDisplayUI : MonoBehaviour
+{
+    //public Text nameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //nameText.text = "EmployeeName";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
